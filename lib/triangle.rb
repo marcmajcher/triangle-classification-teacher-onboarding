8e9a603140064a89c9c0a3893270d2e686054437
Marc Majcher <majcher@gmail.com>
2020-02-07 14:18:51 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    sides = [@a, @b, @c].sort
+    if (@a > 0 && @b > 0 && @c > 0) &&
+       (sides[0] + sides[1] > sides[2])
+      if @a == @b && @b == @c
+        return :equilateral
+      end
+      if @a == @b || @b == @c || @c == @a
+        return :isosceles
+      end
+      return :scalene
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "TRI AGAIN"
+    end
+  end
 end
